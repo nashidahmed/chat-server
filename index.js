7d5374009d689bd085f15dc9c0595592610abc66
Nashid Shah <nashid.ahmed.115@gmail.com>
2023-12-03 04:50:11 -0500

    Added websocket to server code

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 import http from "http"
+import { WebSocketServer, WebSocket } from "ws"
 
-const port = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader("Content-Type", "text/html")
-  res.end("<h1>Hello World</h1>")
+  // Handle HTTP requests if needed
+  res.writeHead(200, { "Content-Type": "text/plain" })
+  res.end("WebSocket server is running")
 })
 
-server.listen(port, () => {
-  console.log(`Server running at port ` + port)
+const wss = new WebSocketServer({ server })
+
+wss.on("connection", (ws) => {
+  console.log("Client connected")
+
+  ws.send("Connected to WebSocket")
+
+  ws.once("message", (name) => {
+    console.log(`Received name: ${name}`)
+
+    // Send a welcome message with the client's name to all connected clients
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        console.log(`New user ${name} joined the chat`)
+        client.send(`New user ${name} joined the chat`)
+      }
+    })
+
+    ws.on("message", (message) => {
+      console.log(`Client says: ${message}`)
+    })
+
+    // Handle WebSocket connection closing
+    ws.on("close", () => {
+      console.log("Client disconnected")
+    })
+  })
+})
+
+server.listen(PORT, () => {
+  console.log(`Server running at port: ${PORT}`)
 })
