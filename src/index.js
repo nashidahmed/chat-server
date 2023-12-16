7f4b0b8c9dcb1bb6245fb663450822e79d8f0967
Nashid Shah <nashid.ahmed.115@gmail.com>
2023-12-16 15:35:41 -0500

    Added domain restriction for connected clients

--- src/index.js
+++ src/index.js
@@ -8,10 +8,26 @@ const server = http.createServer(app)
 const wss = new WebSocketServer({ server })
 
 const PORT = process.env.PORT || 3000
+const allowedDomains = ["https://websocketchat.vercel.app/"]
 let users = []
 
 // Middleware for enforcing HTTPS
 app.use(enforce.HTTPS({ trustProtoHeader: true }))
+
+app.use((req, res, next) => {
+  const origin = req.get("origin")
+
+  if (allowedDomains.includes(origin)) {
+    // Allow the request
+    res.setHeader("Access-Control-Allow-Origin", origin)
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type")
+    next()
+  } else {
+    // Deny the request
+    res.status(403).send("Forbidden")
+  }
+})
 
 app.get("/", (req, res) => {
   res.send("WebSocket server is running")
