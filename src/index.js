8dd20f30d4d9d5078466643aa07e23b6584144c0
Nashid Shah <nashid.ahmed.115@gmail.com>
2023-12-17 15:26:10 -0500

    Added single chat capability

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import http from "http"
 import { WebSocketServer, WebSocket } from "ws"
 import enforce from "express-sslify"
+import User from "./models/user.js"
 
 const app = express()
 const server = http.createServer(app)
@@ -9,6 +10,7 @@ const wss = new WebSocketServer({ server })
 
 const PORT = process.env.PORT || 3000
 const allowedDomains = ["https://websocketchat.vercel.app/"]
+const clients = new Map()
 let users = []
 
 // Middleware for enforcing HTTPS
@@ -34,30 +36,47 @@ app.get("/", (req, res) => {
 })
 
 wss.on("connection", (ws) => {
+  const sendToClient = (pubKey, message) => {
+    const targetClient = clients.get(pubKey)
+
+    if (targetClient) {
+      targetClient.send(message)
+    } else {
+      console.log(`Target client not found.`)
+    }
+  }
+
   const sendToAllClients = (message) => {
     wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
+      if (client.readyState === WebSocket.OPEN) {
         client.send(message)
       }
     })
   }
 
-  ws.once("message", (name) => {
-    users.push({ name, ws })
+  ws.once("message", (message) => {
+    const { name, pubKey } = JSON.parse(message)
+    const newUser = new User(name, pubKey)
+    users = [...users, newUser]
+    clients.set(pubKey, ws)
 
     sendToAllClients(
       JSON.stringify({
         type: "info",
-        value: `${name} joined the chat`,
+        value: users,
       })
     )
 
     ws.on("message", (message) => {
-      const { msg, timestamp } = JSON.parse(message)
-      sendToAllClients(
+      const { msg, from, to, timestamp } = JSON.parse(message)
+      console.log(msg)
+      sendToClient(
+        `${to}`,
         JSON.stringify({
           type: "message",
           name: `${name}`,
+          from: `${from}`,
+          to: `${to}`,
           value: `${msg}`,
           timestamp: `${timestamp}`,
         })
@@ -65,11 +84,14 @@ wss.on("connection", (ws) => {
     })
 
     ws.on("close", () => {
-      users = users.filter((user) => user.ws !== ws)
+      users = users.filter((user) => clients.get(user.pubKey) !== ws)
+      console.log("User left", users)
+      clients.delete(pubKey)
+
       sendToAllClients(
         JSON.stringify({
           type: "info",
-          value: `${name} left the chat`,
+          value: users,
         })
       )
     })
